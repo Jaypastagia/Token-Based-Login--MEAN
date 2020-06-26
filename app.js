24a2c3f241c1971fc52b7051b642c07ce7fd3fc8
jaykumar.pastagia <jaykumar.pastagia@ivlglobal.com>
2020-06-26 22:08:06 +0530

    pre release test

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var employee = require('./routes/employee');
 
 var app = express();
 var router = express.Router();
-// testing standard-version
+// testing standard-version pre release test
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
